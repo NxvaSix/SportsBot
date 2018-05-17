ce7f450750e697f55d761854c08a0ee104b34c1d
speketwome <39380827+speketwome@users.noreply.github.com>
2018-05-17 17:21:39 +0100

    Update SportsBot.py

--- SportsBot.py
+++ SportsBot.py
@@ -1,8 +1,15 @@
+#Import modules here
 import discord
 from discord.ext import commands
 import asyncio
-import time
 import os
+
+"""You cannot use the Time module. Instead to await asyncio.sleep(Seconds), aysncio allows the bot to work because it pauses coroutines 
+however time.sleep pauses the whole bot, which is not what we want.
+
+Also make sure that there are no repeating loops because python will not run the discord bot code if the previos code
+hasn't been executed."""
+
 
 bot = commands.Bot(command_prefix='~')
 bot.remove_command('help')
